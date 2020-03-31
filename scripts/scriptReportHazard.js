c66b60b614b76f2b139a402eb1819e814c0b2437
Jianming Lu <lujianming000@gmail.com>
2020-03-31 03:53:51 -0700

    fixing bugs

--- scripts/scriptReportHazard.js
+++ scripts/scriptReportHazard.js
@@ -1,36 +1,38 @@
-function writeReport(){
-    document.getElementById("reportHazard").addEventListener("submit", function(e){
-
+function writeReport() {
+    document.getElementById("reportHazard").addEventListener("submit", function (e) {
         e.preventDefault();
         let hT = document.getElementById("hazardType").value;
         let hD = document.getElementById("hazardDescription").value;
         let hP = document.getElementById("photoInput").value;
-        db.collection("hazards").add({
-            HazardType:hT,
-            HazardDescription:hD,
-            HazardUser:"John Doe",
-            Position:[100,100]
-        })
-    })
-}
-function showHazards(){
-    db.collection("hazards").onSnapshot(function(snapshot){
-        changes = snapshot.docChanges();
-        changes.forEach(function(change){
-            if(change.type == "added"){
-                console.log(change.doc);
-            }
-            
+        db.collection("hazards").doc("02").set({
+            HazardType: hT,
+            HazardDescription: hD,
+            HazardUser: "John Doe",
+            Position: [100, 100]
+        }).then(function () {
+            window.location.assign("map.html");
         })
     })
 }
 
-function addHazardToMap(hazard){
-    document.getElementById("type").innerHtml=hazard.HazardType;
-    document.getElementById("description").innerHtml=hazard.HazardDescription;
-    document.getElementById("user").innerHtml=hazard.HazardUser;
-    document.getElementById("longitude").innerHtml=hazard.Position[0];
-    document.getElementById("latitude").innerHtml=hazard.Position[1];
+function showHazards() {
+    db.collection("hazards").onSnapshot(function (snapshot) {
+        changes = snapshot.docChanges();
+        changes.forEach(function (change) {
+            if (change.type == "added") {
+                console.log(change.doc);
+            }
+
+        })
+    })
+}
+
+function addHazardToMap(hazard) {
+    document.getElementById("type").innerHtml = hazard.HazardType;
+    document.getElementById("description").innerHtml = hazard.HazardDescription;
+    document.getElementById("user").innerHtml = hazard.HazardUser;
+    document.getElementById("longitude").innerHtml = hazard.Position[0];
+    document.getElementById("latitude").innerHtml = hazard.Position[1];
 
 }
 showHazards();
